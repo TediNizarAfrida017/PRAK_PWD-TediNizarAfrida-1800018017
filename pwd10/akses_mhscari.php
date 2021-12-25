dd6f7bb41de03d613a23ba0820ffee7737fa511c
TediNizarAfrida017 <92085282+TediNizarAfrida017@users.noreply.github.com>
2021-12-25 13:00:14 +0700

    TEMU 10 POSTEST PRAK PWD
    TEMU 10 POSTEST PRAK PWD

--- pwd10/akses_mhscari.php
+++ pwd10/akses_mhscari.php
@@ -1,26 +1,22 @@
+<form action="akses_mhscari.php" method="get">
+	<label>Cari :</label>
+	<input type="text" name="cari">
+	<input type="submit" value="Cari">
+</form>
 <?php
-//membuat link url di getdatamhs.php
-$url = "http://localhost/pwd10/getdatamhs.php";
-//mengambil data dari $url 
+error_reporting(0);
+$cari = $_GET['cari'];
+$url = "http://localhost/pwd10/getcarimhs.php?cari=".$cari;
 $client = curl_init($url);
-//mengembalikan transfer menjadi bentuk String.
 curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
-//menampilkan data dalam $client
 $response = curl_exec($client);
-//menerjemahkan string JSON
 $result = json_decode($response); 
-	//membuat perulangan data
 	foreach ($result as $r) {
 	echo "<p>";
-	//menampilkan data nim
 	echo "NIM : " . $r->nim . "<br />";	
-	//menampilkan data nama
 	echo "Nama : " . $r->nama . "<br />";  
-	//menampilkan data jkel
 	echo "jenis kel : " . $r->jkel . "<br />"; 
-	//menampilkan data alamat
 	echo "Alamat : " . $r->alamat . "<br />"; 
-	//menampilkan data tgllhr
 	echo "Tgl Lahir : " . $r->tgllhr . "<br />"; 
 	echo "</p>";
 }
--- pwd10/getdatamhs.php
+++ pwd10/getdatamhs.php
@@ -1,12 +1,18 @@
 <?php
+//menyertakan file koneksi.php
 require_once "koneksi.php";
+	//query untuk menampilkan data mahasiswa
 	$sql   = "select * from mahasiswa";
+	//menjalankan instruksi query
 	$query = mysqli_query($koneksi,$sql);
-
+	//membuay perulangan nilai array
 	while ($row = mysqli_fetch_assoc($query)) {
 		$data[] = $row;
 	}
+	//mengirimkan data json ke browser
 	header('content-type: application/json'); 
+	//merubah variabel PHP (berisi array) menjadi JSON
 	echo json_encode($data);
+//menutup koneksi database
 mysqli_close($koneksi);
 ?>
